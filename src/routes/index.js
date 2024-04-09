436bde96b2f34adf8f5956d7a8cdee60621e20ab
huy33371 <quochuy1442k2@gmail.com>
2024-04-10 05:41:56 +0700

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import productRoutes from './productRoutes';
+import categoryRoutes from './categoryRoutes';
+import userRoutes from './userRoutes';
+import searchRouter from './searchRoutes';
+import manufacturerRoutes from './manufacturerRoutes';
+
+const router = express.Router();
+
+let initWebRouter = () => {
+
+    // Các routes được xử lý tại đây
+    router.get('/', (req, res) => {
+        return res.send('Hello world')
+    });
+
+    // route search
+    router.use('/search', searchRouter);
+    
+    // Sử dụng các routes
+    router.use('/products', productRoutes);
+    router.use('/categories', categoryRoutes);
+    router.use('/users', userRoutes);
+    router.use('/manufacturers', manufacturerRoutes);
+
+    return router;
+}
+
+module.exports = initWebRouter;
